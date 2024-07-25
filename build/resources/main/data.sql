85dfd6c6b22f5ba5c10a20dcdf26d7608f7cc850
hanesura <51861852+hanesura@users.noreply.github.com>
2024-07-25 15:17:56 +0900

    Delete build directory

--- build/resources/main/data.sql
+++
@@ -1,3 +0,0 @@
-insert into issues(summary, description) values('バグA', 'バグがあります');
-insert into issues(summary, description) values('機能要件B', '機能追加してほし');
-insert into issues(summary, description) values('画面Cが遅い', '早くしてす');
